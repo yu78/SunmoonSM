bbbb5702967f1f91888094a7c201ce579389779d
simvery <sim960425@naver.com>
2018-04-14 22:38:33 +0900

    ok

---
+++ PHPlogin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>프로듀스SM</title>
+  </head>
+  <body>
+  <form method="post" action="process.phh">
+      <label>ID: <input type="text" name="member_id"/></label>
+      <label>PW: <input type="password" name="member_password" /></label>
+      <input type="submit" />
+    </form>
+  </body>
+</html>
