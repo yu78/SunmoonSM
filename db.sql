e45c925e0b20a083c9a037b5e99408373ed5ed94
simvery <sim960425@naver.com>
2018-06-04 21:34:16 +0900

    Add files via upload

--- db.sql
+++ db.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `post_board`
+-- Database: `producesm`
 --
 CREATE DATABASE IF NOT EXISTS `producesm` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `producesm`;
@@ -30,22 +30,23 @@ USE `producesm`;
 -- Table structure for table `board`
 --
 
-CREATE TABLE `board` (
+CREATE TABLE `free_board` (
   `idx` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` text NOT NULL,
   `date` date NOT NULL,
-  `hit` int(11) NOT NULL
+  `hit` int(11) NOT NULL,
+  `file` varchar(100)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `board`
 --
 
-INSERT INTO `board` (`idx`, `name`, `title`, `content`, `date`, `hit`) VALUES
-(1, 'admin', '4월 23일', '4월 23일', '2018-05-02', 69),
-(2, 'admin', '게시판 댓글 카운트', '게시판 댓글 카운트', '2018-05-02', 10);
+INSERT INTO `free_board` (`idx`, `name`, `title`, `content`, `date`, `hit`, `file`) VALUES
+(1, 'admin', '4월 23일', '4월 23일', '2018-05-02', 69, NULL),
+(2, 'admin', '게시판 댓글 카운트', '게시판 댓글 카운트', '2018-05-02', 10, NULL);
 
 -- --------------------------------------------------------
 
@@ -58,24 +59,27 @@ CREATE TABLE `member` (
   `id` varchar(100) NOT NULL,
   `pw` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `student_idx` int(10) NOT NULL,
+  `college_name` varchar(45) NOT NULL,
+  `major_name` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`idx`, `id`, `pw`, `name`, `email`) VALUES
-(1, 'admin', '$2y$10$DTx4VUTjqKQgMBPLotpUKex9VhXSdK0O70BeFMqighRfih2w52IjW', '관리자', 'bgpoilkj@naver.com'),
-(2, '1234', '$2y$10$XWOIe/NMV3/9JyXFLSXhvupq4FWTKyxzlG7yDJh.yeXVqHyh5dfZi', '1234', '1234@naver.com');
+INSERT INTO `member` (`idx`, `id`, `pw`, `name`, `email`, `student_idx`, `college_name`, `major_name`) VALUES
+(1, 'admin', '$2y$10$DTx4VUTjqKQgMBPLotpUKex9VhXSdK0O70BeFMqighRfih2w52IjW', '관리자', 'admin@sunmoon.ac.kr', 1234567899,'관리자', '관리자'),
+(2, '1234', '$2y$10$XWOIe/NMV3/9JyXFLSXhvupq4FWTKyxzlG7yDJh.yeXVqHyh5dfZi', '1234', '1234@sunmoon.ac.kr', 0123456789, '공과대학', '컴퓨터공학부');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reply`
+-- Table structure for table `free_reply`
 --
 
-CREATE TABLE `reply` (
+CREATE TABLE `free_reply` (
   `idx` int(11) NOT NULL,
   `con_num` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -84,22 +88,102 @@ CREATE TABLE `reply` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `reply`
+-- Dumping data for table `free_reply`
 --
 
-INSERT INTO `reply` (`idx`, `con_num`, `name`, `content`, `date`) VALUES
+INSERT INTO `free_reply` (`idx`, `con_num`, `name`, `content`, `date`) VALUES
 (1, 1, 'admin', '댓글!', '2018-05-02 01:04:23'),
 (2, 1, 'admin', '카운트!', '2018-05-02 01:04:26'),
 (3, 2, 'admin', '5월 2일', '2018-05-02 01:04:01');
+
+-- --------------------------------------------------------
+
+
+--
+-- Table structure for table `trade_board`
+--
+
+CREATE TABLE `trade_board` (
+  `idx` int(11) NOT NULL,
+  `trade_header` varchar(45) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `date` date NOT NULL,
+  `hit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `trade_board`
+--
+
+INSERT INTO `trade_board` (`idx`, `trade_header`, `name`, `title`, `content`, `date`, `hit`) VALUES
+(1, '원해요', 'admin', '강의교환', 'test', '2018-05-02', 69),
+(2, '드려요', 'admin', '게시판테스트', 'test', '2018-05-02', 10),
+(3, '교환해요', 'admin', '게시판테스트', 'test', '2018-05-02', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trade_reply`
+--
+
+CREATE TABLE `trade_reply` (
+  `idx` int(11) NOT NULL,
+  `con_num` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `trade_reply`
+--
+
+INSERT INTO `trade_reply` (`idx`, `con_num`, `name`, `content`, `date`) VALUES
+(1, 1, 'admin', '댓글!', '2018-05-02 01:04:23'),
+(2, 1, 'admin', '카운트!', '2018-05-02 01:04:26'),
+(3, 2, 'admin', '5월 2일', '2018-05-02 01:04:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `evaluation`
+--
+
+CREATE TABLE `evaluation` (
+  `idx` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lectureName` varchar(50) NOT NULL,
+  `professorName` varchar(50) NOT NULL,
+  `lectureYear` int(11) NOT NULL,
+  `semesterDivide` varchar(20) NOT NULL,
+  `lectureDivide` varchar(10) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` varchar(2048) NOT NULL,
+  `totalScore` varchar(10) NOT NULL,
+  `creditScore` varchar(10) NOT NULL,
+  `comfortableScore` varchar(10) NOT NULL,
+  `lectureScore` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `evaluation`
+--
+
+INSERT INTO `evaluation` (`idx`, `name`, `lectureName`, `professorName`, `lectureYear`, `semesterDivide`, `lectureDivide`, `title`, `content`, `totalScore`, `creditScore`, `comfortableScore`, `lectureScore`) VALUES
+(1, 'admin', '종합프로젝트', '김봉재', 2018, '1학기', '전공', '최고에요', '갓봉짱짱맨', 'A', 'A', 'A', 'A');
+
+-- --------------------------------------------------------
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `board`
+-- Indexes for table `free_board`
 --
-ALTER TABLE `board`
+ALTER TABLE `free_board`
   ADD PRIMARY KEY (`idx`);
 
 --
@@ -109,19 +193,39 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`idx`);
 
 --
--- Indexes for table `reply`
+-- Indexes for table `free_reply`
 --
-ALTER TABLE `reply`
+ALTER TABLE `free_reply`
   ADD PRIMARY KEY (`idx`);
+
+
+-- Indexes for table `trade_board`
+--
+ALTER TABLE `trade_board`
+  ADD PRIMARY KEY (`idx`);
+
+
+--
+-- Indexes for table `trade_reply`
+--
+ALTER TABLE `trade_reply`
+  ADD PRIMARY KEY (`idx`);
+
+
+-- Indexes for table `evaluation`
+--
+ALTER TABLE `evaluation`
+  ADD PRIMARY KEY (`idx`);
+
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `board`
+-- AUTO_INCREMENT for table `free_board`
 --
-ALTER TABLE `board`
+ALTER TABLE `free_board`
   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 --
@@ -131,11 +235,31 @@ ALTER TABLE `member`
   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 --
--- AUTO_INCREMENT for table `reply`
+-- AUTO_INCREMENT for table `free_reply`
 --
-ALTER TABLE `reply`
+ALTER TABLE `free_reply`
   MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 COMMIT;
+
+
+-- AUTO_INCREMENT for table `trade_board`
+--
+ALTER TABLE `trade_board`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+
+
+--
+-- AUTO_INCREMENT for table `trade_reply`
+--
+ALTER TABLE `trade_reply`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+COMMIT;
+
+
+-- AUTO_INCREMENT for table `trade_board`
+--
+ALTER TABLE `evaluation`
+  MODIFY `idx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- index.php
+++ index.php
@@ -1,54 +1,160 @@
 ﻿<?php include "db.php"; ?>
-<!DOCTYPE html>
-<html lang="ko">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="ko" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="utf-8" />
-	<title>프로듀스SM</title>
-	<meta name="viewport" content="wideth=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/common.css" />
-  <link rel="stylesheet" href="css/top_menu.css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-	 
-<!-- 팝업창 호출 -->
-<script language = "javascript">
-	function showPopup() {
-		//팝업창 가운데 호출 함수가 제대로 안됨 일단은 눈대중으로 가운데 맞춰놨음
-		// var windowWidth = 700; //가로
-		// var windowHeight = 600; //세로
-		// var left = Math.ceil((window.screen.width - windowWidth)/2);
-		// var top = Math.ceil((window.screen.height - windowHeight)/2);
-		// window.open("signUp.html", "a", "width="+windowWidth+, "height="+windowHeight+, "left="+left+, "top="+top+);}
-		window.open("../member/log_in.php", "a", "width=700, height=600, left=430, top=50");}
-</script>
-</head>
-<body>
-	<h3><a href="../index.php"><img src="../img/프로듀스SM로고.png"></a></h3>
-  <br><br><nav id="topMenu">
-		<ul class="center">
-		<li><a class="menuLink" href="../page/evaluation.php">강의평가</a></li>
-		<li><a class="menuLink" href="../page/trade.php">강의교환</a></li>
-		<li><a class="menuLink" href="../page/list.php">자유게시판</a></li>
-		<li><a class="menuLink" onclick="showPopup();">로그인</a></li>
-		</ul>
-  </nav>  <!-- end of menu -->
-	<br><br><h4><img src="../img/본관.png"></h4>
-	<!-- <div id="login_box">
-    <h1>로그인</h1>
-      <h4 id="main_t">게시판을 이용하려면 로그인이 필요합니다.</h4>
-        <form method="post" action="member/login_ok.php">
-    			<table align="center" border="0" cellspacing="0" width="300" id="log_ta">
-            <tr>
-              <td width="130" colspan="1"><input type="text" name="userid" class="inph"></td>
-              <td rowspan="2" align="center" width="100" > <button type="submit" id="btn" >로그인</button></td>
-            </tr>
-            <tr>
-              <td width="130" colspan="1"> <input type="password" name="userpw" class="inph"></td>
-            </tr>
-            <tr>
-              <td colspan="3" align="center" class="mem"><a href="member/member.php">회원가입 하시겠습니까?</a></td>
-            </tr>
-          </table>
-        </form>
-      </div> -->
-    </body>
-  </html>
+	<title>프로듀스SM</title>
+	<meta name="viewport" content="wideth=device-width, initial-scale=1"> <!-- 반응형 -->
+	<link rel="stylesheet" type="text/css" href="css/common.css" />
+	<!-- 합쳐지고 최소화된 최신 CSS -->
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<!-- 부가적인 테마 -->
+	<link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+	<!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+	<script src="../js/bootstrap.min.js"></script>
+	<!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+
+	<!-- 부트스트랩 이미지 슬라이드 -->
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+	<!-- css -->
+	<style>
+	th{
+		text-align: center;
+	}
+	</style>
+	<style>
+	td{
+		text-align: center;
+	}
+	</style>
+	<style>
+	#jb-container {
+		width: 940px;
+		margin: 10px auto;
+		padding: 20px;
+	}
+
+	/* 반응형 */
+	@media ( max-width: 768px ) {
+		#jb-container {
+			width: auto;
+		}
+	}
+		</style>
+
+		<!-- 자바 스크립트 -->
+		<script language = "javascript">
+		// 팝업창 호출
+		function popupLogin() {
+			//팝업창 가운데 호출 함수가 제대로 안됨 일단은 눈대중으로 가운데 맞춰놨음
+			// var windowWidth = 700; //가로
+			// var windowHeight = 600; //세로
+			// var left = Math.ceil((window.screen.width - windowWidth)/2);
+			// var top = Math.ceil((window.screen.height - windowHeight)/2);
+			// window.open("signUp.html", "a", "width="+windowWidth+, "height="+windowHeight+, "left="+left+, "top="+top+);}
+			window.open("../member/login.php", "a", "width=500, height=600, left=550, top=100");
+			}
+			function popupRegister() {
+				window.open("../member/register.php", "a", "width=550, height=600, left=550, top=30");
+			}
+			</script>
+		</head>
+		<body>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			  <a class="navbar-brand"href="../index.php">프로듀스SM</a>
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarColor01">
+			    <ul class="navbar-nav mr-auto">
+			      <li class="nav-item">
+			        <a class="nav-link" href="../page/evaluation.php">강의평가</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="../page/trade.php">강의교환</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="../page/list.php">자유게시판</a>
+			      </li>
+						<?php if(isset($_SESSION['userid'])){	?>
+							<li class="nav-item">
+				        <a class="nav-link" href="../page/mypage.php">마이페이지</a>
+				      </li>
+							<li class="nav-item">
+				        <a class="nav-link" href="../member/logout.php">로그아웃</a>
+				      </li>
+						<?php } else { ?>
+			      <li class="nav-item">
+			        <a class="nav-link" onclick="popupLogin();">로그인</a>
+			      </li>
+						<li class="nav-item">
+			        <a class="nav-link" onclick="popupRegister();">회원가입</a>
+			      </li>
+						<?php } ?>
+			    </ul>
+				</div>
+			</nav>
+
+			<!-- 이미지 슬라이드 -->
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!--페이지-->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>
+				<!--페이지-->
+				<div class="carousel-inner">
+					<!--슬라이드1-->
+					<div class="item active">
+						<img src="../img/본관.png" style="width:100%" alt="First slide">
+						<div class="container">
+							<div class="carousel-caption">
+							</div>
+						</div>
+					</div>
+
+					<!--슬라이드2-->
+					<div class="item">
+						<img src="../img/자연관.png" style="width:100%" data-src="" alt="Second slide">
+						<div class="container">
+							<div class="carousel-caption">
+							</div>
+						</div>
+					</div>
+
+					<!--슬라이드3-->
+					<div class="item">
+						<img src="../img/기숙사.jpg" style="width:100%" data-src="" alt="Third slide">
+						<div class="container">
+							<div class="carousel-caption">
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--이전, 다음 버튼-->
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+
+			<br><br><div class="container">
+				<p class="text-right">최신강의평가<br>
+				<?php
+					$result = mq("SELECT * FROM evaluation order by idx desc limit 3");
+					while($evaluation = mysqli_fetch_array($result)){
+				?>
+				<?php echo $evaluation['lectureName']?><small>(<?php echo $evaluation['professorName']; ?>)</small> 총점 : <span style="color: red;"><?php echo $evaluation['totalScore']; ?></span><br>
+				<?php } ?>
+				</p>
+			</div>
+			<!-- footer -->
+			<div class="container" style="padding-top:100px;">
+				<p class="text-center">Copyright ⓒ 프로듀스SM. All rights reserved.</p>
+			</div>
+		</body>
+	</html>
