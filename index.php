a8472d7a0b6d79a9d5abc6354cabd6ac41c9ca4b
yu78 <sinnakiki@gmail.com>
2018-06-09 04:04:50 +0900

    index 수정
    랭귀지 셀렉트박스, 캐러셀, 최신강의평가 수정

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-﻿<?php include "db.php"; ?>
+<?php include "db.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ko" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 	<title>프로듀스SM</title>
 	<meta name="viewport" content="wideth=device-width, initial-scale=1"> <!-- 반응형 -->
 	<link rel="stylesheet" type="text/css" href="css/common.css" />
+	<link rel="stylesheet" href="../css/board.css" />
 	<!-- 합쳐지고 최소화된 최신 CSS -->
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<!-- 부가적인 테마 -->
@@ -37,26 +37,12 @@
 		margin: 10px auto;
 		padding: 20px;
 	}
-
 	/* 반응형 */
 	@media ( max-width: 768px ) {
 		#jb-container {
 			width: auto;
 		}
 	}
-	/* footer */
-	/* #footer {
-   position:fixed;
-   left:0px;
-   bottom:0px;
-   height:60px;
-   width:100%;
-   background:#EE7785;
-   color: white;
-	} */
-	/* #footer p {
-   text-align: center;
-	} */
 		</style>
 
 		<!-- 자바 스크립트 -->
@@ -64,11 +50,6 @@
 		// 팝업창 호출
 		function popupLogin() {
 			//팝업창 가운데 호출 함수가 제대로 안됨 일단은 눈대중으로 가운데 맞춰놨음
-			// var windowWidth = 700; //가로
-			// var windowHeight = 600; //세로
-			// var left = Math.ceil((window.screen.width - windowWidth)/2);
-			// var top = Math.ceil((window.screen.height - windowHeight)/2);
-			// window.open("signUp.html", "a", "width="+windowWidth+, "height="+windowHeight+, "left="+left+, "top="+top+);}
 			window.open("../member/login.php", "a", "width=500, height=600, left=550, top=100");
 			}
 			function popupRegister() {
@@ -104,17 +85,27 @@
 					 <?php } else { ?>
 						 <?php if($_SESSION['level']==5) { ?>
  							<li class="nav-item">
- 								<a class="nav-link" href="../admin/adminlist.php">관리자</a>
+ 								<a class="nav-link" href="../page/admin/adminlist.php">관리자</a>
  							</li>
  						<?php }?>
-						 <li class="nav-item">
-							 <a class="nav-link" href="../page/mypage.php">마이페이지</a>
-						 </li>
+						 		<li class="nav-item">
+							 		<a class="nav-link" href="../page/mypage.php">마이페이지</a>
+						 		</li>
 						 <li class="nav-item">
 							 <a class="nav-link" href="../member/logout.php">로그아웃</a>
 						 </li>
 						<?php } ?>
 			    </ul>
+					<ul class="navbar-nav mr-auto navbar-right">
+						<li class="nav-item" style="margin-top:10px;">
+							<select style="height:35px;" class="form-control" name="jump" onchange="location.href=this.value">
+								<option value="index.php">Language</option>
+								<option value="index.php">한글</option>
+								<option value="index_en.php">English</option>
+								<option value="index_jp.php">日本語</option>
+							</select>
+						</li>
+					</ul>
 				</div>
 			</nav>
 
@@ -125,12 +116,15 @@
 					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
 					<li data-target="#myCarousel" data-slide-to="1"></li>
 					<li data-target="#myCarousel" data-slide-to="2"></li>
+					<li data-target="#myCarousel" data-slide-to="3"></li>
+					<li data-target="#myCarousel" data-slide-to="4"></li>
+					<li data-target="#myCarousel" data-slide-to="5"></li>
 				</ol>
 				<!--페이지-->
 				<div class="carousel-inner">
 					<!--슬라이드1-->
 					<div class="item active">
-						<img src="../img/본관.png" style="width:100%" alt="First slide">
+						<img src="../img/img1.jpg" style="width:100%" alt="First slide">
 						<div class="container">
 							<div class="carousel-caption">
 							</div>
@@ -139,7 +133,7 @@
 
 					<!--슬라이드2-->
 					<div class="item">
-						<img src="../img/자연관.png" style="width:100%" data-src="" alt="Second slide">
+						<img src="../img/img2.jpg" style="width:100%" data-src="" alt="Second slide">
 						<div class="container">
 							<div class="carousel-caption">
 							</div>
@@ -148,23 +142,80 @@
 
 					<!--슬라이드3-->
 					<div class="item">
-						<img src="../img/기숙사.jpg" style="width:100%" data-src="" alt="Third slide">
+						<img src="../img/img3.jpg" style="width:100%" data-src="" alt="Third slide">
 						<div class="container">
 							<div class="carousel-caption">
 							</div>
 						</div>
 					</div>
+
+					<!--슬라이드4-->
+					<div class="item">
+						<img src="../img/img4.jpg" style="width:100%" data-src="" alt="Fourth slide">
+						<div class="container">
+							<div class="carousel-caption">
+							</div>
+						</div>
+					</div>
+
+					<!--슬라이드5-->
+					<div class="item">
+						<img src="../img/본관.jpg" style="width:100%" data-src="" alt="Fifth slide">
+						<div class="container">
+							<div class="carousel-caption">
+							</div>
+						</div>
+					</div>
+
+					<!--슬라이드6-->
+					<div class="item">
+						<img src="../img/자연관.jpg" style="width:100%" data-src="" alt="Sixth slide">
+						<div class="container">
+							<div class="carousel-caption">
+							</div>
+						</div>
+					</div>
+
 				</div>
 				<!--이전, 다음 버튼-->
 				<a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
 				<a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
 			</div>
+
+			<br>
+			<div class="container" style="margin-bottom:70px;">
+				<p style="text-align:center;font-size:15pt;"><strong>최신강의평가</strong><br></p>
+							<?php
+							$result = mq("SELECT * FROM evaluation order by idx desc limit 3");
+							while($evaluation = mysqli_fetch_array($result)){
+							?>
+							<div class="card bg-light mt-3" style="margin:0 auto;height:auto;width:500px;">
+
+							<a><?php echo $evaluation['lectureName']?>(<?php echo $evaluation['professorName']; ?>)</a>
+							<a>총점 : <font color=red><?php echo $evaluation['totalScore']; ?></font></a>
+							</div>
+							<?php } ?>
+
+				</div>
+
+				<!-- <div class="container" style="margin:0 auto;height:auto;width:600px;">
+        <p style="text-align:center;font-size:15pt;"><strong>최신강의평가</strong><br></p>
+          <div class="card bg-light mt-3">
+           	<div class="card-header bg-light">
+                <#?php
+                  	$result = mq("SELECT * FROM evaluation order by idx desc limit 3");
+                    while($evaluation = mysqli_fetch_array($result)){
+                ?>
+                <#?php echo $evaluation['lectureName']?><small>(<#?php echo $evaluation['professorName']; ?>)</small> 총점 : <span style="color: red;"><#?php echo $evaluation['totalScore']; ?></span><br>
+                <#?php } ?>
+            </div>
+          </div>
+        </div> -->
+			</div>
+
 			<!-- footer -->
 			<div style="position:fixed; left:0px; bottom:0px; height:30px; width:100%; background:rgb(120, 194, 173);(120, 194, 173); color: white;">
 	      <p style="text-align:center; top:10px;">Copyright ⓒ 프로듀스SM. All rights reserved.</p>
 	    </div>
-			<!-- <div class="container" style="padding-top:100px;">
-				<p class="text-center">Copyright ⓒ 프로듀스SM. All rights reserved.</p>
-			</div> -->
 		</body>
 	</html>
