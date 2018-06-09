4c129413fc1c1a091c4de1d774a7eb6cfefe5445
simvery <sim960425@naver.com>
2018-06-09 23:52:46 +0900

    Add files via upload

---
+++ page/evaluation_board/evaluation_modify_ok_jp.php
@@ -0,0 +1,17 @@
+<?php
+	include "../../db.php";
+if(isset($_SESSION['userid'])&&($_SESSION['state']=='1')){
+
+	$bno = $_POST['idx'];
+	$sql = mq("select * from evaluation where idx='$bno';");
+	$evaluation = $sql->fetch_array();
+
+$sql2 = mq("update evaluation set name='".$_SESSION['userid']."',lectureName='".$_POST['lectureName']."',professorName='".$_POST['professorName']."',lectureYear='".$_POST['lectureYear']."',semesterDivide='".$_POST['semesterDivide']."',lectureDivide='".$_POST['lectureDivide']."',title='".$_POST['title']."',content='".$_POST['content']."',totalScore='".$_POST['totalScore']."',creditScore='".$_POST['creditScore']."',comfortableScore='".$_POST['comfortableScore']."',lectureScore='".$_POST['lectureScore']."' where idx = '".$bno."'");
+echo "<script>alert('修正されました。');</script>";
+?>
+<meta http-equiv="refresh" content="0 url=../../evaluation_jp.php?idx=<?php echo $evaluation['idx']; ?> ">
+<?php
+    }else{
+      echo "<script>alert('ログインが必要です。'); location.href='../index_jp.php'; </script>";
+    }
+?>
